b41a9bb2152f6b5a31005396f680056308c6801a
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-19 20:35:24 +0530

    Added:code random coupan

--- src/com/bridgelabz/classtest/ClassTest.java
+++ src/com/bridgelabz/classtest/ClassTest.java
@@ -1,7 +1,24 @@
 package com.bridgelabz.classtest;
 
+import java.util.Scanner;
+
 public class ClassTest {
+    public static void coupanNumber(){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int temp=0;
+        for(int i=1;i<=n;i++){
+            int randm=(int) (Math.random());
+            if(temp!=randm){
+                System.out.println(randm);
+            }
+            temp=randm;
+        }
+
+    }
     public static void main(String[] args) {
         System.out.println("class test");
+
+     coupanNumber();
     }
 }
